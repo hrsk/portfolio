087170dd94ffa312f26c44b90be108b82dedc729
hrsk <hrsk.yegor@gmail.com>
2023-12-05 17:18:53 +0300

    global styles, background image

---
+++ src/styles/global/GlobalStyles.ts
@@ -0,0 +1,23 @@
+import {createGlobalStyle} from "styled-components";
+import background_image from './../../assets/background/background.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-size: cover;
+    background:url(${background_image});
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
