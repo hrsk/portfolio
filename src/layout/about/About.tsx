3da0c8a800fcd5877bd7dd3d0835f4cb4ec6f5a1
hrsk <hrsk.yegor@gmail.com>
2023-12-15 20:35:44 +0300

    refactoring layout about, added google fonts

--- src/layout/about/About.tsx
+++ src/layout/about/About.tsx
@@ -12,29 +12,31 @@ export const About = () => {
                 <Header title={"About Me"}
                         description={"Get to know me"}
                 />
-                <FlexWrapper direction={'row'} align={'center'} columnGap={'59px'} >
-                    {/*<ImageWrapper >*/}
-                        <Image src={ph} alt={'About Picture'}/>
-                    {/*</ImageWrapper>*/}
-                    <DescriptionWrapper>
-                        <Description>
-                            <span>Who am i?</span>
-                            <h1>I'm Yahor Hursky, a Frontend Developer</h1>
-                            <p>
-                                I am a freelancer based in the United Kingdom and i have been building noteworthy UX/UI designs and websites for years, which comply with the latest design trends. I help convert a vision and an idea into meaningful and useful products. Having a sharp eye for product evolution helps me prioritize tasks, iterate fast and deliver faster.
-                            </p>
-                        </Description>
+                <FlexWrapper direction={'row'} align={'center'} columnGap={'59px'}>
+                    <Image src={ph} alt={'About Picture'}/>
+                    <Description>
+                        <span>Who am i?</span>
+                        <h1>I'm Yahor Hursky, a Frontend Developer</h1>
+                        <Text>
+                            I am a freelancer based in the United Kingdom and i have been building noteworthy UX/UI
+                            designs and websites for years, which comply with the latest design trends. I help
+                            convert a vision and an idea into meaningful and useful products. Having a sharp eye for
+                            product evolution helps me prioritize tasks, iterate fast and deliver faster.
+                        </Text>
                         <ContactWrapper>
                             <h4>Name:</h4>
                             <p>Yahor Hursky</p>
                             <h4>Email:</h4>
-                            <p><a href="mailto:gurskygursky@example.com">hrsk.yegor@example.com</a></p>
+                            <p><Link as={'a'} href="mailto:gurskygursky@example.com">hrsk.yegor@example.com</Link></p>
                             <h4>Age:</h4>
                             <p>31</p>
                             <h4>From:</h4>
                             <p>Minsk, Belarus</p>
                         </ContactWrapper>
-                    </DescriptionWrapper>
+                        <LinksWrapper>
+                            <Button as={'a'} href={'/#'}>Download CV</Button>
+                        </LinksWrapper>
+                    </Description>
                 </FlexWrapper>
             </Container>
         </AboutWrapper>
@@ -43,10 +45,14 @@ export const About = () => {
 
 const AboutWrapper = styled.section`
   padding: 15px 0 15px 0;
+  margin-bottom: 120px;
 `
-
+const Text = styled.p`
+  border-bottom: 2px solid rgba(255, 255, 255, .08);
+  margin-bottom: 0;
+  padding-bottom: 36px;
+`
 const Description = styled.div`
-  
   span {
     color: #009e66;
     padding-bottom: 15px;
@@ -56,23 +62,21 @@ const Description = styled.div`
     line-height: 1.6;
     margin-bottom: 16px;
   }
-  p {
-    border-bottom: 2px solid rgba(255,255,255,.08);
-    margin-bottom: 0;
-    padding-bottom: 36px;
-  }
+
 `
 
 const ContactWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-row-gap: 15px;
-  padding: 32px 0;
+  padding: 15px 0;
+  margin-bottom: 4px;
 `
 
 const Image = styled.img`
   object-fit: cover;
   width: 430px;
+  height: 450px;
   max-height: 100%;
   max-width: 100%;
 
@@ -81,12 +85,23 @@ const Image = styled.img`
   padding: 0.25rem;
 `
 
-
-
-const DescriptionWrapper = styled.div`
-  max-width: 70%;
+const Button = styled.button.attrs((props) => ({
+    type: props.type || 'submit'
+}))`
+  margin-right: 24px;
+  padding: 10px 35px;
+  border-radius: 30px;
+  background-color: #009e66;
+  border: 1px solid #009e66;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #fff;
+`
+const Link = styled.link`
+  text-decoration: none;
+  color: #009e66;
+`
+const LinksWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
 `
--- src/styles/global/Global.styled.ts
+++ src/styles/global/Global.styled.ts
@@ -20,7 +20,7 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 15px;
     font-weight: 400;
     line-height: 1.95;
-    font-family: Helvetica, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family: Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -56,7 +56,7 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 32px;
     font-weight: 700;
   }
-  
+
   h2 {
     font-weight: 500;
     line-height: 1.2;
