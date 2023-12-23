79b72516bf9197e05174cd274813969be8272e95
hrsk <hrsk.yegor@gmail.com>
2023-12-23 16:23:08 +0300

    aded social media icons, refactoring, footer layout

---
+++ src/components/footer/social_media/SocialMediaList.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import React from "react";
+import {SocialMediaItem} from "./SocialMediaItem";
+
+export const socialMediaItems: Array<SocialMediaList> = [
+    {id: 1, title: 'facebook', link: 'https://facebook.com/'},
+    {id: 2, title: 'github', link: 'https://github.com/hrsk'},
+    {id: 3, title: 'linkedin', link: 'https://linkedin.com/'},
+    {id: 4, title: 'telegram', link: 'https://t.me/rzhavoevremya'},
+    {id: 5, title: 'twitter', link: 'https://twitter.com/'},
+]
+
+type SocialMediaList = {
+    id: number
+    title: string
+    link: string
+}
+
+export const SocialMediaList = () => {
+    return (
+        <SocialMediaListWrapper>
+            {socialMediaItems.map((socialItem) => {
+                return <SocialMediaItem key={socialItem.id} title={socialItem.title} link={socialItem.link}/>
+            })}
+        </SocialMediaListWrapper>
+    )
+}
+const SocialMediaListWrapper = styled.ul`
+    display: flex;
+    flex-direction: row;
+    height: 24px;
+    column-gap: 25px;
+    right: 0;
+`
--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,22 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/global/Theme";
+import {Container} from "../../components/Container";
+import {SocialMediaList} from "../../components/footer/social_media/SocialMediaList";
 
 export const Footer = () => {
     return (
         <FooterWrapper>
-            <span>Copyright © 2024 All rights reserved.</span>
+            <Container>
+                <FlexWrapper direction={'row'} align={'center'} justify={'space-evenly'}>
+                    <Copyright>Copyright © 2024 Yahor Hurski. All rights reserved.</Copyright>
+                    <SocialMediaList/>
+                </FlexWrapper>
+            </Container>
         </FooterWrapper>
     );
 }
 
-const FooterWrapper = styled.small`
-  background-color: #0f0f11;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding: 10px;
-
-  span {
-    color: white;
-  }
+const FooterWrapper = styled.footer`
+    padding: 15px;
 `
+
+const Copyright = styled.small`
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.4;
+    letter-spacing: 0.1em;
+    color: ${theme.colors.fonts.text}
+`
+
